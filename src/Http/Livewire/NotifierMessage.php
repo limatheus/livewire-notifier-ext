cc961bec817547836782d2ae2a775bf0eb535f23
Matheus Lima <contato@matheuslima.com.br>
2021-08-13 10:30:54 -0300

    Added action type

--- src/Http/Livewire/NotifierMessage.php
+++ src/Http/Livewire/NotifierMessage.php
@@ -28,29 +28,50 @@ class NotifierMessage extends Component
      * @var boolean
      */
     public $closable = true;
+
+    /**
+     * Url for the action
+     *
+     * @var String
+     */
+    public $action_url;
+
+    /**
+     * Label for the action button
+     *
+     * @var String
+     */
+    public $action_label;
+
     /**
      * Mount lifecycle action
      *
      * @return void
      */
-    public function mount(){
+    public function mount()
+    {
         $this->duration = $this->duration ?? config('livewire-notifier.duration');
         $this->closable = $this->closable ?? config('livewire-notifier.closable');
-        $this->msgClass = $this->msgClass ?? config('livewire-notifier.types.' . ($this->message['type'] ?? 'default') . '.msgClass',config('livewire-notifier.types.default.msgClass'));
-        $this->progressClass = $this->progressClass ?? config('livewire-notifier.types.' . ($this->message['type'] ?? 'default') . '.progressClass',config('livewire-notifier.types.default.progressClass'));
+        $this->msgClass = $this->msgClass ?? config('livewire-notifier.types.' . ($this->message['type'] ?? 'default') . '.msgClass', config('livewire-notifier.types.default.msgClass'));
+        $this->progressClass = $this->progressClass ?? config('livewire-notifier.types.' . ($this->message['type'] ?? 'default') . '.progressClass', config('livewire-notifier.types.default.progressClass'));
+        $this->action_url = $this->action_url ?? null;
+        $this->action_label = $this->action_label ?? null;
+
     }
-    
+
     public function render()
     {
         $this->message = array_merge([
-            'text'=>'',
+            'text' => '',
             'title' => '',
             'icon' => '',
             'type' => 'success',
             'duration' => $this->duration,
-            'msgClass' =>  $this->msgClass,
-            'progressClass' =>  $this->progressClass,
+            'msgClass' => $this->msgClass,
+            'progressClass' => $this->progressClass,
             'closable' => $this->closable,
+            'action_url' => $this->action_url,
+            'action_label' => $this->action_label
         ], $this->message);
         return view('livewire-notifier::livewire.notifier-message');
     }

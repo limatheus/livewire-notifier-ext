8eeafeb8d678f73a595fa259341b3735a424d7a0
Matheus Lima <contato@matheuslima.com.br>
2021-08-18 16:20:05 -0300

    Added some extras

--- src/Http/Livewire/NotifierMessage.php
+++ src/Http/Livewire/NotifierMessage.php
@@ -44,6 +44,20 @@ class NotifierMessage extends Component
     public $action_label;
 
     /**
+     * Type to action style
+     *
+     * @var String
+     */
+    public $action_type;
+
+    /**
+     * Play sound on message display
+     *
+     * @var boolean
+     */
+    public $play_sound = false;
+
+    /**
      * Mount lifecycle action
      *
      * @return void
@@ -56,6 +70,8 @@ class NotifierMessage extends Component
         $this->progressClass = $this->progressClass ?? config('livewire-notifier.types.' . ($this->message['type'] ?? 'default') . '.progressClass', config('livewire-notifier.types.default.progressClass'));
         $this->action_url = $this->action_url ?? null;
         $this->action_label = $this->action_label ?? null;
+        $this->action_type = $this->action_type ?? null;
+        $this->play_sound = $this->play_sound ?? false;
 
     }
 
@@ -71,7 +87,9 @@ class NotifierMessage extends Component
             'progressClass' => $this->progressClass,
             'closable' => $this->closable,
             'action_url' => $this->action_url,
-            'action_label' => $this->action_label
+            'action_label' => $this->action_label,
+            'action_type' => $this->action_type,
+            'play_sound' => $this->play_sound
         ], $this->message);
         return view('livewire-notifier::livewire.notifier-message');
     }
